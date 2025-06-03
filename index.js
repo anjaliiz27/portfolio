0b0a26a7e0e0a0e3284f89a973d41eb66548bd1e
anjaliiz27 <anjali.purpink74@gmail.com>
2025-06-03 13:12:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,8 +16,9 @@ for(var i=0;i<navLinks.length;i++){
 }
 
 document.querySelector("button").addEventListener("click", function(event){
-    event.preventDefault();
-    alert("submitted succesfully!");
+     event.preventDefault();
+     alert("submitted succesfully!");
+   
      document.getElementById("name").value=" ";
      document.getElementById("email").value=" ";
      document.getElementById("message").value=" ";
